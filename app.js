14b98bbd3b48c08b98318bf3703d04d2b06d2ac6
Sebastiano-Pizzata <rsebastiano23@gmail.com>
2025-02-12 14:26:10 +0100

    ADD: folders and files through the terminal

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 2000;
+
+// const postsRouter = require("./routers/posts.js")
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("Server del mio Blog")
+})
+
+// app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+    console.log(`server ${port}`)
+})
